12ed3f87b0b8841f2cfb319870ca1d664b52ef70
ChrisDev <Christianpeya@hotmail.com>
2025-04-27 09:10:45 -0600

    Se agregaron configuraciones

--- src/main/java/com/chris/venta_de_boletos/HelloController.java
+++ src/main/java/com/chris/venta_de_boletos/HelloController.java
@@ -1,14 +1,46 @@
 package com.chris.venta_de_boletos;
 
+import com.chris.venta_de_boletos.Service.GestorBoletos;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.layout.GridPane;
+import org.controlsfx.control.spreadsheet.Grid;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private GridPane gridTable;
+
+    private GestorBoletos gestorBoletos = new GestorBoletos();
+    private Button[][] botones = new Button[7][10];
+
+    public void initialize() {
+
+        List<String> asientos = gestorBoletos.insertarboletos(7, 10);
+
+        int aux = 0;
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 10; j++) {
+                botones[i][j] = new Button();
+                gridTable.add(botones[i][j], i, j);
+                botones[i][j].setText(asientos.get(aux));
+                aux++;
+                botones[i][j].setStyle("-fx-background-color: #0f0");
+
+            }
+        }
+
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void detected(MouseDragEvent event) {
+
     }
+
+
+
 }
