beea508f46439fdb61c62bfe5c8fe1df7f71a0da
Axelllel <axelrebop@outlook.com>
2025-04-28 23:26:21 -0600

    agregue unas clases

--- src/main/java/com/chris/venta_de_boletos/Service/GestorBoletos.java
+++ src/main/java/com/chris/venta_de_boletos/Service/GestorBoletos.java
@@ -7,17 +7,18 @@ import java.util.*;
 
 public class GestorBoletos {
 
+    private static final int maxReportes = 100;
+
     private LinkedList<Boletos> boletosGuadados;
     private Boolean[][] asientosDisponibles;
     private HashMap<String, Integer> categoriasSeleccionadas;
-    private Queue<Reportes> reportesVentas;
-
+    private Cola reportesVentas;
 
     public List<String> insertarboletos(int fila, int columna) {
         boletosGuadados = new LinkedList<>();
         asientosDisponibles = new Boolean[fila][columna];
         categoriasSeleccionadas = new HashMap<>();
-        reportesVentas = new LinkedList<>();
+        reportesVentas = new Cola(maxReportes);
 
         categoriasSeleccionadas.put("VIP", 1000);
         categoriasSeleccionadas.put("GENERAL", 500);
@@ -25,16 +26,20 @@ public class GestorBoletos {
 
         List<String> asientoBotones = new ArrayList<>();
 
-
         char Asiento = 'A';
         for (int i = 0; i < fila; i++) {
             for (int j = 0; j < columna; j++) {
                 String categoria;
-                if (i < 3) categoria = "VIP";
-                if (i > 2 && i < 5) categoria = "GENERAL";
-                else categoria = "PREFERENCIAL";
+                if (i < 3) {
+                    categoria = "VIP";
+                }
+                if (i > 2 && i < 5) {
+                    categoria = "GENERAL";
+                } else {
+                    categoria = "PREFERENCIAL";
+                }
 
-                asientoBotones.add(Asiento + "" +(j + 1));
+                asientoBotones.add(Asiento + "" + (j + 1));
 
                 Boletos boletos = new Boletos(
                         UUID.randomUUID().toString(),
@@ -50,15 +55,29 @@ public class GestorBoletos {
         return asientoBotones;
     }
 
+    private void reporteDeVenta(List<Boletos> boletosVendidos, String categoria) {
+        double ingresoTotal = 0;
+        for (Boletos boleto : boletosVendidos) {
+            ingresoTotal += boleto.getPrecio();
 
-    private void reporteDeVenta(){
+        }
+        Reportes nuevoReporte = new Reportes(java.time.LocalDateTime.now(),
+                categoria,
+                boletosVendidos.size(),
+                ingresoTotal,
+                boletosVendidos);
+
+        reportesVentas.insert(nuevoReporte);
 
     }
 
-    private void guardarReporteArchivo(){
+    private void guardarReporteArchivo() {
+        while (!reportesVentas.isEmpty()) {
+            Reportes reporte = reportesVentas.remove();
+            com.chris.venta.de.boletos.utils.Guardar_reporte.guardarEnArchivo(reporte, "reportes_ventas.txt");
+
+        }
 
     }
-
-
 
 }
